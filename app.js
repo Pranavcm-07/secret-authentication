abce367d24033d86202ac862f25eddf6c105b458
pranav <pranavcm602@gmail.com>
2023-03-24 11:11:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require('express')
+const bodyparser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.set('view engine','ejs')
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB',{useNewUrlParser: true})
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+})
+
+userSchema.plugin(encrypt,{secret:process.env.SECRETS , encryptedFields: ['password']})
+
+const User = mongoose.model('User',userSchema)
+
+app.get('/',function(req,res){
+    res.render('home')
+})
+
+app.get('/login',function(req,res){
+    res.render('login')
+})
+
+
+app.get('/register',function(req,res){
+    res.render('register')
+})
+
+
+app.post('/register',function(req,res){
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+
+    newUser.save()
+        .then(function(){
+            res.render('secrets')
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+})
+
+app.post('/login',function(req,res){
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email:username})
+        .then(function(found){
+            if (found.password === password){
+                res.render('secrets')
+            }
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+})
+
+
+
+
+
+
+
+app.listen(3000,function(req,res){
+    console.log('server is running on port 3000')
+})
